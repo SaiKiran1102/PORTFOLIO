b37fb7186d6565f0bdfc3356158c4cfa0b4ed0e5
sai kiran <saikiran30112003@gmail.com>
2025-04-13 15:40:55 +0530

    Fix Vite base path for Render

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:"/sai-portfolio"
 
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "./", // 👈 set to relative path
 });
